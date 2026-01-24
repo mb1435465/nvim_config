2e706281f7c8693f1c31dcee479285fd99498f5c
Malcolm Brown <malcolm.brown@brcon.net>
2026-01-24 13:11:13 -0500

    testing fugitive in neovim

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,24 +1,49 @@
 return {
-    "tpope/vim-fugitive",
-    config = function ()
-        vim.keymap.set("n", "<leader>gg", ":Git<CR>")
-        vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<CR>")
-        vim.keymap.set("n", "<leader>gl", ":Git blame<CR>")
-        vim.keymap.set("n", "<leader>gm", ":Git mergetool<CR>")
+	"tpope/vim-fugitive",
+	config = function()
+		vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gD", vim.cmd.Gdiffsplit)
+		vim.keymap.set("n", "<leader>gB", function()
+			vim.cmd.Git("blame")
+		end)
+		vim.keymap.set("n", "<leader>gm", function()
+			vim.cmd.Git("mergetool")
+		end)
+		vim.keymap.set("n", "<leader>gc", function()
+			vim.cmd.Git("commit")
+		end)
+		vim.keymap.set("n", "<leader>ga", function()
+			vim.cmd.Git("add --all")
+		end)
+		vim.keymap.set("n", "<leader>gd", function()
+			vim.cmd.Git("diff")
+		end)
+		vim.keymap.set("n", "<leader>gl", function()
+			vim.cmd.Git("log")
+		end)
+		vim.keymap.set("n", "<leader>gp", function()
+			vim.cmd.Git("push")
+		end)
+		vim.keymap.set("n", "<leader>gP", function()
+			vim.cmd.Git("pull --rebase")
+		end)
+		vim.keymap.set("n", "<leader>gA", function()
+			vim.cmd.Git("add " .. vim.fn.expand("%"))
+		end)
 
-        vim.opt.diffopt:append("algorithm:patience")
-        vim.opt.diffopt:append("indent-heuristic")
+		vim.opt.diffopt:append("algorithm:patience")
+		vim.opt.diffopt:append("indent-heuristic")
 
-        vim.keymap.set("n", "<leader>gj", ":diffget //3<CR>")
-        vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>")
+		vim.keymap.set("n", "<leader>gj", ":diffget //3<CR>")
+		vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>")
 
-        vim.cmd([[
+		vim.cmd([[
             function! ConflictStats()
                 let l:conflict_pattern = "^<<<<<<< "
                 let l:conflicts = search(l:conflict_pattern, 'n')
                 echo "Remaining conflicts: " . l:conflicts
             endfunction
         ]])
-        vim.keymap.set("n", "<leader>go", ":call ConflictStats()<CR>")
-    end
+		vim.keymap.set("n", "<leader>go", ":call ConflictStats()<CR>")
+	end,
 }
