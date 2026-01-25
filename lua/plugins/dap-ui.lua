d32ba745e795f25f5342c8f2078a77b34b0d3227
Malcolm Brown <malcolm.brown@brcon.net>
2026-01-25 12:01:23 -0500

    updates

---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,32 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		dapui.setup({})
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<leader>ue", function()
+			dapui.eval()
+		end)
+		vim.keymap.set("n", "<leader>uf", function()
+			dapui.float_element()
+		end)
+	end,
+}
