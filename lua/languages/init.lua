62990296bd0411133ff9b1ac015f9ed89715c641
Malcolm Brown <malcolm.brown@brcon.net>
2026-01-25 12:09:46 -0500

    updates

--- lua/languages/init.lua
+++ lua/languages/init.lua
@@ -1,21 +1,21 @@
 local lsp_utils = require("core.lsp")
 
-vim.lsp.config("*", { 
-    capabilities = lsp_utils.capabilities(),
+vim.lsp.config("*", {
+	capabilities = lsp_utils.capabilities(),
 })
 
 local cwDir = vim.fn.stdpath("config")
-local files = vim.split(vim.fn.glob(cwDir .. "/lua/languages/*"), '\n', {trimempty=true})
+local files = vim.split(vim.fn.glob(cwDir .. "/lua/languages/*"), "\n", { trimempty = true })
 
 for _, f in pairs(files) do
-    local file = vim.fn.fnamemodify(f, ":t")
-    local language = vim.fn.fnamemodify(file, ":r")
+	local file = vim.fn.fnamemodify(f, ":t")
+	local language = vim.fn.fnamemodify(file, ":r")
 
-    if language == "init" then
-        goto continue
-    end
+	if language == "init" then
+		goto continue
+	end
 
-    require("languages."..language)
+	require("languages." .. language)
 
-    ::continue::
+	::continue::
 end
