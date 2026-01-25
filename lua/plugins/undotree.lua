af0f3124d2487abb4f848ce39c31de7074cf85ab
Malcolm Brown <malcolm.brown@brcon.net>
2026-01-24 20:34:48 -0500

    added == 1 for check rather than bool

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -3,9 +3,9 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
 		-- for windows
-        if (vim.fn.has("win32")) then
-            vim.g.undotree_DiffCommand = "FC"
-        end
+		if vim.fn.has("win32") == 1 then
+			vim.g.undotree_DiffCommand = "FC"
+		end
 		vim.g.undotree_SetFocusWhenToggle = 1
 		vim.opt.undofile = true
 		vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
